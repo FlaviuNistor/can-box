4790dc8c073723ceca32c6669973f27b46fce689
nistorf <flaviu.nistor@continental-corporation.com>
2024-07-17 10:16:11 +0300

    add files for logging option
    create initial c and h files for logging
    add build support to include the new created source file

---
+++ src/log.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+/* added to support mem leack debugging */ 
+#include <mcheck.h>
+
+/* add local headers */
+#include "log.h"
+
