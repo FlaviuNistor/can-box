11576acab101c5440ef1e8f342e7b489ca8b0bc1
nistorf <flaviu.nistor@continental-corporation.com>
2024-07-17 10:51:32 +0300

    create also dump file

--- src/log.c
+++ src/log.c
@@ -9,13 +9,21 @@
 #include "log.h"
 
 FILE *log_file = NULL;
+FILE *dump_file = NULL;
 
-unsigned int create_log_file(char * file_name){
-    static char fname[83]; /* suggested by -Wformat-overflow= */
-    sprintf(fname, "%s.log", file_name);  
-	log_file = fopen(fname, "w");
+unsigned int create_log_files(char * file_name){
+    static char log_name[83]; /* suggested by -Wformat-overflow= */
+    static char dump_name[83]; /* suggested by -Wformat-overflow= */
+    sprintf(log_name, "%s.log", file_name);
+    sprintf(dump_name, "%s-dump.log", file_name);  
+	log_file = fopen(log_name, "w");
 	if (!log_file) {
             perror("logfile");
+            return 1;
+	}
+    dump_file = fopen(dump_name, "w");
+	if (!dump_file) {
+            perror("dumpfile");
             return 1;
 	}
     return 0;
--- src/log.h
+++ src/log.h
@@ -2,6 +2,7 @@
 #define LOG_H
 
 extern FILE *log_file;
-unsigned int create_log_file(char * file_name);
+extern FILE *dump_file;
+unsigned int create_log_files(char * file_name);
 
 #endif /* LOG_H */
