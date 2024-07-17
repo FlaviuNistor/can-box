670de7498ddac4d0b0deab655b3ac680b0155170
nistorf <flaviu.nistor@continental-corporation.com>
2024-07-17 10:43:06 +0300

    fix typo bug

--- src/log.c
+++ src/log.c
@@ -5,19 +5,18 @@
 /* added to support mem leack debugging */ 
 #include <mcheck.h>
 
-FILE *logfile = NULL;
+/* add local headers */
+#include "log.h"
+
+FILE *log_file = NULL;
 
 unsigned int create_log_file(char * file_name){
     static char fname[83]; /* suggested by -Wformat-overflow= */
     sprintf(fname, "%s.log", file_name);  
-	logfile = fopen(fname, "w");
-	if (!logfile) {
+	log_file = fopen(fname, "w");
+	if (!log_file) {
             perror("logfile");
             return 1;
 	}
     return 0;
 }
-
-/* add local headers */
-#include "log.h"
-
