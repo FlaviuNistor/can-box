dcd8f3933d5e190e34cf812bfe7a8f57163c4b20
nistorf <flaviu.nistor@continental-corporation.com>
2024-07-22 16:39:12 +0300

    fix compile error

--- src/log.h
+++ src/log.h
@@ -10,12 +10,12 @@ struct timestamp {
     unsigned int minutes;
     unsigned int seconds;
     unsigned int miliseconds;
-}
+};
 
 unsigned int create_log_files(char * file_name);
 void log_can_frame_to_dump(struct canfd_frame * can_frame);
 void print_can_frame_to_console(struct canfd_frame * can_frame);
 uint64_t get_current_timestamp();
-void populate_timestamp(struct timestamp * ts, uint64_t milisec)
+void populate_timestamp(struct timestamp * ts, uint64_t milisec);
 
 #endif /* LOG_H */
